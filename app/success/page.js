b55af168125ceb0949430e57ed437aa755a47f21
mprojr <martinprojcs@gmail.com>
2024-07-16 01:16:03 -0500

    Update page.js

--- app/success/page.js
+++ app/success/page.js
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export default function SuccessPage() {
     return (
-        <div>
-            Nice!
-            <Link href={'/'}>Back home</Link>
+        <div className="p-4 m-2">
+            <p className="flex justify-center">Nice!</p>
+            <div className="flex justify-center px-2 mt-6">
+              <button className="border p-2 hover:scale-105 duration-200 hover:bg-blue-400">
+                <Link className="flex justify-center" href={'/'}>Back home</Link>
+              </button>
+            </div>
         </div>
     )
 }
