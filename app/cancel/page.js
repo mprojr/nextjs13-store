d05d486e801999b1b01809ab1b913a2823bcf049
mprojr <martinprojcs@gmail.com>
2024-07-22 17:23:13 -0500

    Final Page Bug Fix

--- app/cancel/page.js
+++ app/cancel/page.js
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export default function CancelPage() {
     return (
-        <div>
-            No Problem
-            <Link>Back home</Link>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+            <h1 className="text-2xl font-bold mb-4">No Problem</h1>
+            <Link href="/">
+                <a className="text-[#76b4c7] hover:text-black text-lg font-semibold">
+                    Back home
+                </a>
+            </Link>
         </div>
     )
 }
