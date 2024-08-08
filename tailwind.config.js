eb8ad9ce8af1bee87c764170d5517742c6553b9b
mprojr <martinprojcs@gmail.com>
2024-08-07 20:02:04 -0500

    Bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: false,
-
   theme: {
     extend: {
       backgroundImage: {
