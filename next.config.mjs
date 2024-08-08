923ee667778f0dab299c461749ca45861d7c25c8
mprojr <martinprojcs@gmail.com>
2024-08-07 20:43:09 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,7 @@ const nextConfig = {
   images: {
     domains: ['files.stripe.com'],
   },
-  env: {
-    MONGODB_USER: process.env.MONGODB_USER,
-    MONGODB_PASS: process.env.MONGODB_PASS,
-    MONGODB_CLUSTER: process.env.MONGODB_CLUSTER,
-    MONGODB_DB: process.env.MONGODB_DB,
-  },
   webpack: (config, { isServer }) => {
-    // Add a rule for handling images
     config.module.rules.push({
       test: /\.(png|jpe?g|gif|svg)$/i,
       use: [
